3d9165f48ec9fbe608c4396acef55cdbc303e4b2
Sattha Puangput <sattha.puangput@gmail.com>
2017-09-16 10:13:01 +0700

    Finished - charity list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.json(
+        [
+            {
+                "id": 0,
+                "name": "Ban Khru Noi",
+                "logo_url": "http://rkdretailiq.com/news/img-corporate-baankrunoi.jpg"
+            },
+            {
+                "id": 1,
+                "name": "Habitat for Humanity Thailand",
+                "logo_url": "http://www.adamandlianne.com/uploads/2/2/1/6/2216267/3231127.gif"
+            },
+            {
+                "id": 2,
+                "name": "Paper Ranger",
+                "logo_url": "https://myfreezer.files.wordpress.com/2007/06/paperranger.jpg"
+            },
+            {
+                "id": 3,
+                "name": "Makhampom",
+                "logo_url": "http://www.makhampom.net/makhampom/ppcms/uploads/UserFiles/Image/Thai/T14Publice/2554/January/Newyear/logoweb.jpg"
+            }
+        ]
+    );
 });
 
 module.exports = router;
